e472ca79cef48dc60236ca9d7f955bb034f29bdb
binodkhadkainfo <techbinodkhadka@gmail.com>
2026-02-17 19:03:05 +0545

    Update 13_next.py

--- 13_next.py
+++ 13_next.py
@@ -2,11 +2,17 @@
 
 marks = int(input("Enter Your Marks"))
 
-if (marks>=90 and marks<=80):
+if (marks>=90 and marks<=100):
+    print ("A+")
+elif (marks>= 80 and marks<90):
     print ("A")
-elif (marks>= 70 and marks<=60):
+elif (marks>=70 and marks<80):
+    print ("B+")
+elif (marks>=60 and marks<70):
     print ("B")
-elif (marks>=50 and marks<=40):
+elif(marks>=50 and marks<60):
+    pritn ("C+")
+elif(marks>=40 and marks<50):
     print ("C")
 else:
-    print ("D")
+    print ("D+")
