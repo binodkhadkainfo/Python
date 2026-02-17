cf375fbf789e46fca15a6ff9cf96d472f5853bd9
binodkhadkainfo <techbinodkhadka@gmail.com>
2026-02-17 19:04:18 +0545

    Update 13_next.py

--- 13_next.py
+++ 13_next.py
@@ -11,7 +11,7 @@ elif (marks>=70 and marks<80):
 elif (marks>=60 and marks<70):
     print ("B")
 elif(marks>=50 and marks<60):
-    pritn ("C+")
+    print ("C+")
 elif(marks>=40 and marks<50):
     print ("C")
 else:
