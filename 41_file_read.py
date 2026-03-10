e09b330c9303bc9e418ec185162e0d7cd17ca1f5
binodkhadkainfo <techbinodkhadka@gmail.com>
2026-03-10 14:03:20 +0545

    Change file name from 'file.txt' to '41_myfile.txt'

--- 41_file_read.py
+++ 41_file_read.py
@@ -1,5 +1,6 @@
 # Open the file named "file.txt"
-f = open("file.txt", "r") # "r" means read mode (this is the default mode in Python)
+f = open("41_myfile.txt", "r") # "r" means read mode (this is the default mode in Python)
 data = f.read()
 print(data)
 f.close()
+
